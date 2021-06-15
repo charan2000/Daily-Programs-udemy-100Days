4f7da5cea87a60dc3af43a88c840e52735c056a2
charan2000 <44344358+charan2000@users.noreply.github.com>
2021-06-15 23:27:06 +0530

    Easy Mode done for password checking; some debugging needed

--- d4_passwordgenerator.py
+++ d4_passwordgenerator.py
@@ -3,5 +3,28 @@ letters = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n',
 numbers = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9']
 symbols = ['!', '#', '$', '%', '&', '(', ')', '*', '+']
 
-def assign(l,n,s):
-    random.r
+n_of_letters = int(input("How many letters do U need in ur password: \n"))
+n_of_nums = int(input("How many Numbers do U need in ur password: \n"))
+n_of_symbols = int(input("How many Symbols do U need in ur password: \n"))
+
+final_password=""
+
+for i in range(1,n_of_letters+1):
+    final_password += random.choices(letters)
+
+for i in range(1,(n_of_nums+1)):
+    final_password += random.choices(numbers)
+
+for i in range(1,(n_of_symbols+1)):
+    final_password += random.choices(symbols)
+
+
+print(final_password)
+
+
+
+
+
+
+
+
