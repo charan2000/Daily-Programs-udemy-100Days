8a585736dbe020a9b53d4ff44f7b4825ba063799
charan2000 <44344358+charan2000@users.noreply.github.com>
2021-06-18 20:59:54 +0530

    Final Thoughts

--- d4_passwordgenerator.py
+++ d4_passwordgenerator.py
@@ -1,4 +1,5 @@
 import random
+from typing import final
 letters = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z']
 numbers = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9']
 symbols = ['!', '#', '$', '%', '&', '(', ')', '*', '+']
@@ -10,16 +11,16 @@ n_of_symbols = int(input("How many Symbols do U need in ur password: \n"))
 final_password=""
 
 for i in range(1,n_of_letters+1):
-    final_password += random.choices(letters)
+    final_password += random.choice(letters)
 
 for i in range(1,(n_of_nums+1)):
-    final_password += random.choices(numbers)
+    final_password += random.choice(numbers)
 
 for i in range(1,(n_of_symbols+1)):
-    final_password += random.choices(symbols)
+    final_password += random.choice(symbols)
 
-
-print(final_password)
+#final_result = random.shuffle(final_password)
+#print(final_result)
 
 
 
