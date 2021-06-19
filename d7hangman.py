94e5cf9a000ccc357faf4d4422d123ed5a6e45fb
charan2000 <44344358+charan2000@users.noreply.github.com>
2021-06-19 07:37:04 +0530

    Step-2 of 5 done

---
+++ d7hangman.py
@@ -0,0 +1,25 @@
+import random
+
+## Creating a word list ##
+word_list = ["Berry","Jerry","Ferry","dragonfruit","mobile","laptop","building","jewellery","knowledge"]
+choosen_word = random.choice(word_list)
+print(choosen_word)
+user_word=""
+display=""
+for i in choosen_word:
+    display+="_"
+for i in choosen_word:
+    input_letter = input("Enter a letter that might fit in the blanks: \n").lower()
+    if input_letter in choosen_word:
+        user_word += input_letter
+    else:
+        print("Sry the letter u choose is not fit for the Word :(\n Try again \n")
+
+    
+
+
+
+if choosen_word == user_word:
+    print("You Won")
+else:print("You Lost")
+
