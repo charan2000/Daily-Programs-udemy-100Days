9feaf0d9fd42cf9e14f84795d4531bb1abe6ebe5
charan2000 <charanbhavisetti2000@gmail.com>
2021-07-02 21:33:25 +0530

    Completed the Project</>

--- Projects/Cesar Cipher/encryption.py
+++ Projects/Cesar Cipher/encryption.py
@@ -4,34 +4,43 @@ alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n'
 
 def decrypt(alphabet, text, shift):
     decrypted_word = ""
-    for i in range(len(text)):
-        shifted_index = alphabet.index(text[i])+25-shift+1
-        if (shifted_index > 25):
-            decrypted_word += alphabet[shifted_index - 26]
-        else:
-            decrypted_word += alphabet[shifted_index]
+    for i in text:
+        if i in alphabet:
+            shifted_index = alphabet.index(i)+25-shift+1
+            if (shifted_index > 25):
+                decrypted_word += alphabet[shifted_index - 26]
+            else:
+                decrypted_word += alphabet[shifted_index]
+        else:decrypted_word+=i
     return decrypted_word
 
 
 def encrypt(alphabet, text, shift):
     encrypted_word = ""
-    for i in range(len(text)):
-        shifted_index = alphabet.index(text[i]) + shift
-        if (shifted_index > 25):
-            encrypted_word += alphabet[shifted_index - 26]
-        else:
-            encrypted_word += alphabet[shifted_index]
-
+    for i in text:
+        if i in alphabet:
+            shifted_index = alphabet.index(i) + shift
+            if (shifted_index > 25):
+                encrypted_word += alphabet[shifted_index - 26]
+            else:
+                encrypted_word += alphabet[shifted_index]
+        else:encrypted_word+=i
     return encrypted_word
 
 
 #direction = input("Type 'encode' to encrypt, type 'decode' to decrypt:\n")
-direction = input(
-    "Which direction of Cesar-Cipher: encrypt or decrypt ").lower()
-text = input("Type your message:\n").lower()
-shift = int(input("Type the shift number:\n"))
-if direction == "encrypt":
-    result = encrypt(alphabet, text, shift)
-else:
-    result = decrypt(alphabet, text, shift)
-print(f"Your {direction}ed text is:", result)
+flag = True
+while flag:
+    direction = input("Which direction of Cesar-Cipher: encrypt or decrypt: ").lower()
+    text = input("Type your message:\n").lower()
+    shift = int(input("Type the shift number:\n"))
+    shift = shift % 26
+    if direction == "encrypt":
+        result = encrypt(alphabet, text, shift)
+    else:
+        result = decrypt(alphabet, text, shift)
+    print(f"Your {direction}ed text is:", result)
+    descison = input("Enter yes to Continue and no to stop: ").lower()
+    if descison=="no":
+        flag=False
+        print("GoodBye: ")
