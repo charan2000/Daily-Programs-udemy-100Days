3d99524cd1c77b46a29386256eded0f807306acd
charan2000 <charanbhavisetti2000@gmail.com>
2022-06-02 23:29:21 +0530

    Fresh start 50 states quiz

--- Day-25/us-states-game-start/us-states-game-start/main.py
+++ Day-25/us-states-game-start/us-states-game-start/main.py
@@ -7,8 +7,17 @@ screen.addshape(image)
 
 turtle.shape(image)
 
+# THe 50 states dataset
+import pandas
+
+states = pandas.read_csv("50_states.csv")
+
+#print(states["state"])
+
+states_list = states["state"].to_list()
+answer_state = screen.textinput(title="Guess the State", prompt="What's the next States Name ").upper()
+
+if answer_state in states_list:
+    print(answer_state)
+
 screen.exitonclick()
-dfs
-
-
-fs
