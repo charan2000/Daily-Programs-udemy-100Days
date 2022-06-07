8593aa759c0bcbe1898938fb028771efc0a39604
charan2000 <charanbhavisetti2000@gmail.com>
2022-06-08 00:05:36 +0530

    Restored the Project 50 States

--- Day-25/us-states-game-start/us-states-game-start/main.py
+++ Day-25/us-states-game-start/us-states-game-start/main.py
@@ -1,4 +1,6 @@
 import turtle
+import pandas
+
 
 screen = turtle.Screen()
 screen.title("U.S States Game")
@@ -8,17 +10,39 @@ screen.addshape(image)
 turtle.shape(image)
 
 # THe 50 states dataset
-import pandas
 
 
 states = pandas.read_csv("50_states.csv")
 
-#print(states["state"])
+# print(states["state"])
 
 states_list = states["state"].to_list()
-answer_state = screen.textinput(title="Guess the State", prompt="What's the next States Name ").title()
+print(len(states_list))
+correct_count = 0
+guessed_states = []
 
-if answer_state in states_list:
-    print(answer_state)
+while len(guessed_states) < 50:
 
-screen.exitonclick()
+    answer_state = str(screen.textinput(title="Guess the State Name", prompt=f"{len(guessed_states)} of 50 States guessed")).title()
+
+    if answer_state == "Exit":
+        missing_states = []
+        for st in states_list:
+            if st not in guessed_states:
+                missing_states.append(st)
+        new_data = pandas.DataFrame(missing_states)
+        new_data.to_csv("states_to_learn.csv")
+        break
+
+    if answer_state in states_list:
+        guessed_states.append(answer_state)
+        #print(answer_state)
+        t = turtle.Turtle()
+        t.hideturtle()
+        t.penup()
+        stat_cord = states[states.state == answer_state]
+        t.goto(int(stat_cord.x), int(stat_cord.y))
+        t.write(answer_state)
+
+
+
