423cd69ae684d4588229476274ed17b987d80051
charan2000 <charanbhavisetti2000@gmail.com>
2022-06-09 01:26:59 +0530

    Completed Day 26

--- Day-26/NATO-alphabet-start/main.py
+++ Day-26/NATO-alphabet-start/main.py
@@ -10,24 +10,25 @@ for (key, value) in student_dict.items():
     pass
 
 import pandas
-student_data_frame = pandas.DataFrame(student_dict)
-
-#Loop through rows of a data frame
-for (index, row) in student_data_frame.iterrows():
-    #Access index and row
-    #Access row.student or row.score
-    print(index, row)
-    pass
 
 # Keyword Method with iterrows()
 # {new_key:new_value for (index, row) in df.iterrows()}
 
+data = pandas.read_csv("nato_phonetic_alphabet.csv")
+#print(data)
+
 #TODO 1. Create a dictionary in this format:
 {"A": "Alfa", "B": "Bravo"}
 
-dict_of_df = {letter: word for (letter,word) in student_data_frame.iterrows()}
+dict_of_data = {row.letter: row.code for (index, row) in data.iterrows()}
 
-print(dict_of_df)
+#print(dict_of_data)
 
 #TODO 2. Create a list of the phonetic code words from a word that the user inputs.
+
+name = input("Enter the Name: ").upper()
+
+final_list = [dict_of_data[letter] for letter in name]
+
+print(final_list)
 
